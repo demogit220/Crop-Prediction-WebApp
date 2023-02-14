3b91cf38e841e09b3470041f1cfc71435a764d3c
demogit220 <gauravkumar_20329@aitpune.edu.in>
2023-02-14 22:56:39 +0530

    Comments removed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import model
-#write code for flask app which uses get.html for getting input and post.html for displaying output and uses predictor.py for prediction
+
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -40,8 +40,6 @@ def post_output():
         data['price'] = result[2]
         print(data)
 
-        # print("test")
-        # result = ""
         return render_template('result.html', result=data)
     return render_template('get.html')
 
@@ -60,10 +58,10 @@ def post_svm_output():
         n = request.form['n']
         k = request.form['k']
         data = {
-            #make whole string lowercase and just keep first character as capital
+            
             'state': request.form['state'].lower().capitalize(),
             'district': request.form['district'].lower().capitalize(),
-            #capitalize first letter of each word and keep rest as lowercase there may have multiple words in season
+            
             'soil': request.form['season'].title(),
             'place': place,
             'season': season,
@@ -73,16 +71,15 @@ def post_svm_output():
             'n': int(n),
             'k': int(k)
         }
-        # print(data)
+        
         result = model.predict_crop(1,data)
-        # print(result)
+        
         data['crop'] = result[0][0]
         data['temp'] = int(result[1][0][0])
         data['price'] = result[2]
         print(data)
 
-        # print("test")
-        # result = ""
+       
         return render_template('result.html', result=data)
     return render_template('get.html')
 
